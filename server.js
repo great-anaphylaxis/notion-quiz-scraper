0e98f5129dfaf5ecc4231e67e3045a9dd1597464
Jhareign <74128037+Jhareign@users.noreply.github.com>
2025-01-30 10:03:17 +0800

    progress! using md

--- server.js
+++ server.js
@@ -1,26 +1,43 @@
 require("dotenv").config();
 const express = require("express");
+const { NotionToMarkdown } = require("notion-to-md");
 const app = express();
 
 const { Client } = require("@notionhq/client");
 const notion = new Client({ auth: process.env.NOTION_KEY });
+const n2m = new NotionToMarkdown({ notionClient: notion });
 
-let blocks = [];
+const databaseId = '18049dfa184480a0b021c04ff6726c72';
+
+let pages = [];
+
+async function markdown(page_id) {
+    const mdblocks = await n2m.pageToMarkdown(page_id);
+    const mdString = n2m.toMarkdownString(mdblocks);
+    
+    return mdString;
+}
+
+function formatData(pages) {
+    
+}
 
 async function getDatabase(callback) {
-    const databaseId = '18049dfa184480a0b021c04ff6726c72';
     const response = await notion.databases.query({ database_id: databaseId });
     
     for (let i = 0; i < response.results.length; i++) {
         const id = response.results[i].id;
-        const page = await notion.blocks.children.list({ block_id: id });
+        //const page = await notion.blocks.children.list({ block_id: id });
+
+        const page = await markdown(id)
         
-        blocks.push(page.results)
+        pages.push(page)
 
     }
 
+    formatData(pages)
 
-    console.log(blocks);
+    console.log(pages);
     callback();
 }
 
@@ -33,7 +50,7 @@ app.use(express.static("public"));
 
 app.get("/", function(request, response) {
     getDatabase(() => {
-        response.send(blocks);
+        response.send(pages);
     });
 });
 
